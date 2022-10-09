86dbb0c549f381a98b445b1a3cc9f803b1e2f7ac
tama3rdGen <tamatomo.21@gmail.com>
2022-10-09 11:15:39 -0500

    minor update

--- MD_postProcessing/main.py
+++ MD_postProcessing/main.py
@@ -3,7 +3,7 @@ import plot
 
 ## --------------   From MD simulation   -------------- ##
 MDdata=TiO2.TiO2()
-MDdata.sizeSet(4)       # 1:1nm, 2:2nm, 3:0.6nm, 4:3nm
+MDdata.sizeSet(1)       # 1:1nm, 2:2nm, 3:0.6nm, 4:3nm
 MDdata.tempSet(1000)
 MDdata.mapping()
 
@@ -15,6 +15,6 @@ MDdata.critical()
 #plot.probabilityMap(MDdata)
 #plot.flagMap(MDdata)
 #plot.bindingLengthMap(MDdata,300)
-#plot.BLmappingTemp(MDdata)
+plot.BLmappingTemp(MDdata,300,1200)
 #plot.betaTemp(MDdata,200,1600)
 #plot.enhanceTemp(MDdata,200,1600)
